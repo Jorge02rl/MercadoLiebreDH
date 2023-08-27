ddccdeb5e3eaeefcd4fc00c22706ad48ca41937e
Jorge02rl <jorgerodrigo2598mail.com>
2023-08-27 18:18:44 -0300

    subiendo proyecto a github

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3030
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('servidor corriendo en el puerto: ' + PORT));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+app.get('/Crea-tu-cuenta', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views','register.html'));
+});
+
+app.post('/Crea-tu-cuenta', (req, res) => {
+    res.redirect('/register.html');
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views','login.html'));
+});
+
+app.post('/login', (req, res) => {
+    res.redirect('/login.html');
+})
