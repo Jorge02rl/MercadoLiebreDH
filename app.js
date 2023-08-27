93be0924dec9c3d4457d47a44309679af9623d60
Jorge02rl <jorgerodrigo2598mail.com>
2023-08-27 18:27:21 -0300

    config render

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 3030
+const port = process.env.PORT || 3001;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log('servidor corriendo en el puerto: ' + PORT));
+app.listen(port, () => console.log('servidor corriendo en el puerto: ' + PORT));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
